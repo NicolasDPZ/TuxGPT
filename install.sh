eddc3ce7e913cc68229342eeefe5522d0baba242
NicolasDPZ <nicolas.perdigonz@javeriana.edu.co>
2025-12-25 22:03:21 -0500

    ahora si

--- install.sh
+++ install.sh
@@ -13,16 +13,28 @@ fi
 
 echo "Distro: $DISTRO"
 
-
-
 if ! command -v ollama &> /dev/null; then
     echo "not Ollama"
-    if [ "$DISTRO" = "arch" ]; then
-        echo "sudo pacman -S ollama"
-    else
-        echo "curl -fsSL https://ollama.com/install.sh | sh"
+
+if [ "$DISTRO" = "debian" ]; then
+    if ! command -v curl &> /dev/null; then
+        echo "installing curl..."
+        sudo apt update
+        sudo apt install -y curl
     fi
-    exit 1
+
+    if ! command -v ollama &> /dev/null; then
+        echo "installing Ollama..."
+        curl -fsSL https://ollama.com/install.sh | sh
+    fi
+fi
+
+if [ "$DISTRO" = "arch" ]; then
+    if ! command -v ollama &> /dev/null; then
+        echo "install Ollama:"
+        echo "sudo pacman -S ollama"
+        exit 1
+    fi
 fi
 
 python3 -m venv venv
